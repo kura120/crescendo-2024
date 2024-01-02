a89b9d3f10a6f1754c520edb9baabd168db50943
allthesquares <allthesquares@allthesquares.dev>
2024-01-02 15:21:57 -0500

    Update robot.py

--- practice/robot.py
+++ practice/robot.py
@@ -1,1 +1,13 @@
 # THIS IS OUR PRACTICE PROJECT TO SPIN A MOTOR!!!!!!!!
+
+import robotpy, wpilib, wpilib.drive, rev
+import math
+
+
+class MyRobot(wpilib.TimedRobot):
+
+    def robotInit(self):
+        return super().robotInit()
+
+
+
