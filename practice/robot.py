e36cbce9138d47aeacf130abe677fbcc35aab7af
Lehansa M <lehansam@gmail.com>
2024-01-04 15:41:48 -0500

    Update robot.py

--- practice/robot.py
+++ practice/robot.py
@@ -6,13 +6,15 @@ import math, time
 
 class MyRobot(wpilib.TimedRobot):
     brushless = rev.CANSparkMaxLowLevel.MotorType.kBrushless
-    
+   
+
     def robotInit(self):
         self.motor = rev.CANSparkMax(1, self.brushless) 
         self.motorEncoder = self.motor.getEncoder()
         self.motor.setIdleMode(rev.CANSparkMax.IdleMode.kBrake)
-
         self.motorEncoder.setPosition(0)
+        self.joystick = wpilib.Joystick(0)  
+
 
     def robotPeriodic(self):
         wpilib.SmartDashboard.putNumber("Encoder", self.motorEncoder.getPosition())
@@ -23,8 +25,18 @@ class MyRobot(wpilib.TimedRobot):
 
 
     def testPeriodic(self):
-        self.motor.set(0.1)
-        
+        if abs(self.joystick.getY()) > 0.1: #Analog input with floats
+
+            self.motor.set(self.joystick.getY())
+        else:
+             self.motor.set(0)
+
+
+        if self.joystick.getRawButton(1):
+            self.motor.set(0.1)
+        else:
+            self.motor.set(0)
+
 
 
 if __name__ == "__main__":
