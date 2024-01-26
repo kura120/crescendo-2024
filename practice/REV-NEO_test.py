3050ce8a639f01aa312ba63f59f8293639b4a1e0
Ryan Ruiz <allthesquares@allthesquares.dev>
2024-01-26 01:26:16 -0500

    Added NEO and CIM test code, fixed an error in CIM code

--- practice/REV-NEO_test.py
+++ practice/REV-NEO_test.py
@@ -1,7 +1,7 @@
 '''
-Programming a CIM motor
-CIM motors are the larger black motors connected to a Talon SRX from CTRE.
-These motors are brushed and will be used in the event REV NEOs are scarce.
+Programming a NEO motor with Encoder support
+NEO motors are the small black motors connected to the REV Spark Max. These motors are brushless.
+You can easily identify the NEO because its name is in big bold text on the motor itself.
 
 Things to know:
 Please be using the latest version of RobotPy. If you are not, run either of these commands
@@ -12,20 +12,22 @@ Please be using the latest version of RobotPy. If you are not, run either of the
     Windows: py -3 -m pip install --upgrade robotpy                 # If needed, specify version of Python, for python.org installs.
              python -m pip install --upgrade robotpy                # Run if you installed Python from the Windows Store.
 
-Make sure the following package is installed or up-to-date: robotpy-ctre
+Make sure the following package is installed or up-to-date: robotpy-rev
 
 When testing, rename this file to robot.py and make sure a joystick is connected to port 0.
-
 '''
 
-import robotpy, wpilib, wpilib.drive, phoenix5 
-
+import robotpy, wpilib, wpilib.drive, rev 
 
 class MyRobot(wpilib.TimedRobot):
     def robotInit(self):
-        self.CIM = phoenix5.TalonSRX(1)         # Replace ID with another number if 1 does not work.
+        # I understand that identifier for the motor type is long. I highly recommend assigning it to a variable "brushless"
+        self.NEO = rev.CANSparkMax(1, rev.CANSparkLowLevel.MotorType.kBrushless)
         self.joystick = wpilib.Joystick(0)
+        self.NEO_Encoder = self.NEO.getEncoder()
 
     def testPeriodic(self):
         if self.joystick.axisGreaterThan(0, 0.1):
-            self.CIM.set(self.joystick.getY())
+            if self.NEO_Encoder.getPosition() < 100 and self.NEO_Encoder.getPosition() >= 0:
+                # The motor will stop if you move it too much.
+                self.NEO.set(self.joystick.getY())
