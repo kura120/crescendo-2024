0e4a57c73b536d0f86b9a03e2ee6f0674ec1f538
Ryan Ruiz <allthesquares@allthesquares.dev>
2024-02-01 18:05:12 -0500

    Removed venvs.

--- crescendo-2024-venv/Lib/site-packages/phoenix5/_cci_sim/include/ctre/phoenix/cci/PDP_CCI.h
+++
@@ -1,7 +0,0 @@
-/* Copyright (C) Cross The Road Electronics 2024 */
-#pragma once
-
-#include "ctre/phoenix/cci/CCI.h"
-#include "ctre/phoenix/ErrorCode.h"
-
-CCIEXPORT ctre::phoenix::ErrorCode c_PDP_GetValues(int deviceID, double *voltage, double currents[], int currentCapacity, int *currentsFilled);
