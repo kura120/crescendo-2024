b2c6db09e8bdec19fa84926760b3e6f10ebe47ca
Team 3340 <magnegeeks3340@gmail.com>
2024-02-05 15:59:20 -0500

    Added timed code for CIM motor

---
+++ DemoCode/Controllling Motors/Timers/TimedCIM.py
@@ -0,0 +1,21 @@
+
+import robotpy, wpilib, phoenix5   
+
+class MyRobot(wpilib.TimedRobot):
+    def robotInit(self):
+        self.motor_timer = wpilib.Timer()
+        self.CIM = phoenix5.TalonSRX(1)         # Replace ID with another number if 1 does not work.
+        self.joystick = wpilib.Joystick(1)
+
+    def testInit(self):
+        self.motor_timer.start()
+
+    def testPeriodic(self):
+        if self.motor_timer.get() > 0.5:
+            self.CIM.set(phoenix5.ControlMode.PercentOutput, 0)
+        else:
+            self.CIM.set(phoenix5.ControlMode.PercentOutput, 0.05)
+        
+    def testExit(self):
+        self.motor_timer.stop()
+        self.motor_timer.reset()
--- robot/robot.py
+++
@@ -1,9 +0,0 @@
-# TODO: insert robot code here
-import robotpy, wpilib, phoenix5 
-
-class MyRobot(wpilib.TimedRobot):
-    def robotInit(self):
-        self.CIM = phoenix5.TalonSRX(1)
-
-    def robotPeriodic(self):
-        print(self.CIM.getSelectedSensorPosition(1))
