0cc490a70f29e0f69e1f543197b62f6932fd7be3
Team 3340 <magnegeeks3340@gmail.com>
2024-02-06 16:59:53 -0500

    a

--- deploy/robot.py
+++ deploy/robot.py
@@ -1,21 +1,36 @@
+'''
+Programming a NEO motor with Encoder support
+NEO motors are the small black motors connected to the REV Spark Max. These motors are brushless.
+You can easily identify the NEO because its name is in big bold text on the motor itself.
 
-import robotpy, wpilib, phoenix5   
+Things to know:
+Please be using the latest version of RobotPy. If you are not, run either of these commands
+    macOS: python3 -m pip install --upgrade robotpy
+           python3.XX -m pip install --upgrade robotpy              # Replace XX with your version of Python (rec. 3.11)
+           python -m pip install --upgrade robotpy 
+
+    Windows: py -3 -m pip install --upgrade robotpy                 # If needed, specify version of Python, for python.org installs.
+             python -m pip install --upgrade robotpy                # Run if you installed Python from the Windows Store.
+
+Make sure the following package is installed or up-to-date: robotpy-rev
+
+When testing, rename this file to robot.py and make sure a joystick is connected to port 0.
+'''
+
+import robotpy, wpilib, wpilib.drive, rev 
 
 class MyRobot(wpilib.TimedRobot):
     def robotInit(self):
-        self.motor_timer = wpilib.Timer()
-        self.CIM = phoenix5.TalonSRX(1)         # Replace ID with another number if 1 does not work.
-        self.joystick = wpilib.Joystick(1)
-
-    def testInit(self):
-        self.motor_timer.start()
+        # I understand that identifier for the motor type is long. I highly recommend assigning it to a variable "brushless"
+        self.NEO = rev.CANSparkMax(1, rev.CANSparkLowLevel.MotorType.kBrushless)
+        self.joystick = wpilib.Joystick(0)
+        self.NEO_Encoder = self.NEO.getEncoder()
+        self.NEO_Encoder.setPosition(0)
 
     def testPeriodic(self):
-        if self.motor_timer.get() > 0.5:
-            self.CIM.set(phoenix5.ControlMode.PercentOutput, 0)
-        else:
-            self.CIM.set(phoenix5.ControlMode.PercentOutput, 0.05)
-        
-    def testExit(self):
-        self.motor_timer.stop()
-        self.motor_timer.reset()
+            print(self.NEO_Encoder.getPosition())
+            if self.NEO_Encoder.getPosition() < 100 and self.NEO_Encoder.getPosition() >= 0:
+                self.NEO_Encoder.getPosition()
+                self.NEO.set(0.25)
+            else:
+                 self.NEO.set(0)
